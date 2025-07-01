bf9e5f3f5c6bcb4f9597e5dfdd11783da1be0c6a
Olivier Kessler <olivier.kessler@protonmail.com>
2025-07-01 09:57:09 +0200

    Basically Hello world

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main(){
+	fmt.Println("Hello world")
+	cmd := exec.Command("ls", "-la")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+}
